fe864ff815df60de89e17e769b3d3f394c09de35
OPENGA_Dev_hllee-at-900616242684 <hl.lee@quintet.co.kr>
2022-03-25 11:50:07 +0900

    header 수정

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,19 +1,47 @@
 import styles from '@/styles/layouts/Header.module.scss'
 import { useCallback, useEffect, useState } from 'react'
 import { Manager } from 'socket.io-client'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import { Avatar, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { deepOrange } from '@mui/material/colors';
+
+const avatarSize = {
+  width: '30px',
+  height: '30px'
+}
+
+const logoutBtn = {
+  color: '#fff',
+  bgColor: '#73c78a',
+  hoverColor: '#7fdb98'
+}
+
+const useStyles = makeStyles(() => ({
+  orangeAvatar: {
+    backgroundColor: deepOrange[500],
+    width: avatarSize.width,
+    height: avatarSize.height
+  },
+  logoutBtn: {
+    color: logoutBtn.color,
+    backgroundColor: logoutBtn.bgColor,
+    '&:hover': {
+      backgroundColor: logoutBtn.hoverColor,
+    }
+  }
+}));
 
 const Header = () => {
+  const classes = useStyles();
   const [count, setCount] = useState(0);
-
   useEffect(() => {
     socketInitializer()
   }, [])
-  
+
   const socketInitializer = async () => {
-    console.log('socketInitializer')
     const url = `http://localhost:9000`
     const manager = new Manager(url)
-    
     const socket = manager.socket('/');
     socket.on('connect', () => {
       console.log('connected')
@@ -22,10 +50,20 @@ const Header = () => {
       setCount(res.count)
     })
   }
+
   return (
-    <header className={styles.header}>
-      <div style={{color: 'red'}}>
-        {count}
+    <header className={styles['header']}>
+      <div className={styles['left-area']}>
+      </div>
+      <div className={styles['right-area']}>
+        <div className={styles['alarm-container']}>
+          <NotificationsNoneIcon className={styles['alarm-bell']} />
+          <div className={styles['count']}>{count}</div>
+        </div>
+        <div className={styles['avatar-container']}>
+          <Avatar alt="H.ONE" className={classes.orangeAvatar}>H</Avatar>
+        </div>
+        <Button variant="contained" disableElevation size={'small'} className={classes.logoutBtn}>로그아웃</Button>
       </div>
     </header>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@mui/styles'
 
 class MyDocument extends Document {
 
@@ -16,3 +18,22 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+  originalRenderPage({
+    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+  });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
